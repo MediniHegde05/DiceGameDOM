d3231ebb735fa12528e69b43b3b49506b441de50
MediniHegde05 <121850926+MediniHegde05@users.noreply.github.com>
2023-01-11 18:17:07 +0530

    update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Palyer 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
  else if (randomNumber2>randomNumber1) {
-    document.querySelector("h1").innerHTML = " Palyer 2 Wins!🚩";
+    document.querySelector("h1").innerHTML = " Player 2 Wins!🚩";
 }
  else {
     document.querySelector("h1").innerHTML = "It's a Draw!!";
